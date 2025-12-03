f09af525d25559984375c2954dac951dc96956c6
azwarsalal <azwar.salal92@gmail.com>
2025-12-03 11:05:19 +0530

    add employee model with schema and migration file

---
+++ app/Models/Employee.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'department_id', 
+        'first_name', 
+        'last_name', 
+        'email', 
+        'dob'
+    ];
+
+    public function department()
+    {
+       // return $this->belongsTo(Department::class);
+    }
+
+    public function phones()
+    {
+        //return $this->hasMany(EmployeePhone::class);
+    }
+
+    public function addresses()
+    {
+       // return $this->hasMany(EmployeeAddress::class);
+    }
+}
