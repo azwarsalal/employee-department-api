9e9112e4b87d145812609694df24e724c53cef22
azwarsalal <azwar.salal92@gmail.com>
2025-12-03 11:22:29 +0530

    create employee address model and migration

---
+++ app/Models/EmployeeAddress.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class EmployeeAddress extends Model
+{
+     use HasFactory;
+
+    protected $fillable = [
+        'employee_id', 
+        'address_line1', 
+        'address_line2', 
+        'city', 
+        'state', 
+        'country', 
+        'zip_code'
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
